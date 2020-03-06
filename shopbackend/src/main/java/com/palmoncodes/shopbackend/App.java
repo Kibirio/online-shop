bc23ea1357f1d3da6b5477f5073903c2a0a96994
kibirio <ruiruharrison047@gmail.com>
2020-03-06 12:10:24 +0300

    Project setup successful

---
+++ shopbackend/src/main/java/com/palmoncodes/shopbackend/App.java
@@ -0,0 +1,13 @@
+package com.palmoncodes.shopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
